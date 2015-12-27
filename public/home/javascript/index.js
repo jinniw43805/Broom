5893b2f6fd21d16de61b2fd94567d3e5a252b83d
tonyk <jinniw43805@gmail.com>
2015-12-27 16:58:18 +0800

    add : text animation

---
+++ public/home/javascript/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $("#typed").typed({
+        // strings: ["Typed.js is a <strong>jQuery</strong> plugin.", "It <em>types</em> out sentences.", "And then deletes them.", "Try it out!"],
+        stringsElement: $('#typed-strings'),
+        typeSpeed: 30,
+        backDelay: 500,
+        loop: false,
+        contentType: 'html', // or text
+        // defaults to false for infinite loop
+        loopCount: false
+    });
+});
