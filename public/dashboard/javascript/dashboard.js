77cd50a4e920506c6c9a6c5019440b936385dcf1
tonyk <jinniw43805@gmail.com>
2016-01-01 13:43:01 +0800

    add :basic cocopad function complete!!!!!!!

--- public/dashboard/javascript/dashboard.js
+++ public/dashboard/javascript/dashboard.js
@@ -1,40 +1,54 @@
 $(document).ready(function() {
-    var regDiaLoader = (function() {
-        if(isRegCompletely===true){
 
-        }else{
-            console.log("showReg");
-            $('#myModal').modal('toggle');
-            // $('#example').tooltip(options)
 
+    noteDataBaseRegister=  (function() {
+        var getExampleRef= function () {
+
+            var ref = new Firebase('https://coconutdata.firebaseio.com/');
+            var hash = window.location.hash.replace(/#/g, '');
+            if (hash) {
+                ref = ref.child(hash);
+            } else {
+                ref = ref.push(); // generate unique location.
+                window.location = window.location + '#' + ref.key(); // add it as a hash to the URL.
+            }
+            if (typeof console !== 'undefined')
+                console.log('Firebase data: ', ref.toString());
+            return ref;
         }
+        var helloworld = function(){
+            return "helloworld";
+        }
+        return{
+            getExampleRef:getExampleRef,
+            helloworld:helloworld
+        };
     }());
 
-
     hello = function (){
-        function init() {
-          //// Initialize Firebase.
-          var firepadRef = getExampleRef();
-          // TODO: Replace above line with:
-          // var firepadRef = new Firebase('<YOUR FIREBASE URL>');
 
-          firepadRef.path.o[0]="-K6Z7eYU_ij57V0cW1C3"
-          console.log(firepadRef.path.o[0]);
+    };
 
+    $('.RenderNote').click(function(event) {
+        console.log("are u call me ?"+this.id);
 
-          //// Create CodeMirror (with lineWrapping on).
-          var codeMirror = CodeMirror(document.getElementById('firepad-container'), { lineWrapping: true });
+        cocoPadRef.path.o[0]= this.id ;
+        //// Create CodeMirror (with lineWrapping on).
 
-          //// Create Firepad (with rich text toolbar and shortcuts enabled).
-          var firepad = Firepad.fromCodeMirror(firepadRef, codeMirror,
-              { richTextToolbar: true, richTextShortcuts: true });
+        var container =document.getElementById('firepad-container'); 
 
-          //// Initialize contents.
-          firepad.on('ready', function() {
-            if (firepad.isHistoryEmpty()) {
-              firepad.setHtml(
-                  '<span style="font-size: 24px;">Rich-text editing with <span style="color: red">Firepad!</span></span><br/>\n' +
-                  '<br/>' +
+        container.innerHTML="";
+
+        var codeMirror = CodeMirror(container, { lineWrapping: true });
+
+        var cocopad = Firepad.fromCodeMirror(cocoPadRef, codeMirror,
+        { richTextToolbar: true, richTextShortcuts: true });
+
+        cocopad.on('ready', function() {
+            if (cocopad.isHistoryEmpty()) {
+                cocopad.setHtml(
+                    '<span style="font-size: 24px;">Rich-text editing with <span style="color: red">cocopad!</span></span><br/>\n' +
+                    '<br/>' +
                   '<div style="font-size: 18px">' +
                   'Supports:<br/>' +
                   '<ul>' +
@@ -61,60 +75,47 @@ $(document).ready(function() {
                   '</ul>' +
                   '</div>');
             }
-          });
+        });
 
-          // An example of a complex custom entity.
-          firepad.registerEntity('checkbox', {
-            render: function (info, entityHandler) {
-              var inputElement = document.createElement('input');
-              inputElement.setAttribute('type', 'checkbox');
-              if(info.checked) {
-                inputElement.checked = 'checked';
-              }
-              inputElement.addEventListener('click', function () {
-                entityHandler.replace({checked:this.checked});
-              });
-              return inputElement;
+        cocopad.registerEntity('checkbox', {
+            render: function(info, entityHandler) {
+                var inputElement = document.createElement('input');
+                inputElement.setAttribute('type', 'checkbox');
+                if (info.checked) {
+                    inputElement.checked = 'checked';
+                }
+                inputElement.addEventListener('click', function() {
+                    entityHandler.replace({
+                        checked: this.checked
+                    });
+                });
+                return inputElement;
             }.bind(this),
-            fromElement: function (element) {
-              var info = {};
-              if(element.hasAttribute('checked')) {
-                info.checked = true;
-              }
-              return info;
+            fromElement: function(element) {
+                var info = {};
+                if (element.hasAttribute('checked')) {
+                    info.checked = true;
+                }
+                return info;
             },
-            update: function (info, element) {
-              if (info.checked) {
-                element.checked = 'checked';
-              } else {
-                element.checked = null;
-              }
+            update: function(info, element) {
+                if (info.checked) {
+                    element.checked = 'checked';
+                } else {
+                    element.checked = null;
+                }
             },
-            export: function (info) {
-              var inputElement = document.createElement('checkbox');
-              if(info.checked) {
-                inputElement.setAttribute('checked', true);
-              }
-              return inputElement;
+            export: function(info) {
+                var inputElement = document.createElement('checkbox');
+                if (info.checked) {
+                    inputElement.setAttribute('checked', true);
+                }
+                return inputElement;
             }
-          });
-        }
-        function getExampleRef() {
-          var ref = new Firebase('https://coconutdata.firebaseio.com/');
-          var hash = window.location.hash.replace(/#/g, '');
-          if (hash) {
-            ref = ref.child(hash);
-          } else {
-            ref = ref.push(); // generate unique location.
-            window.location = window.location + '#' + ref.key(); // add it as a hash to the URL.
-          }
-          if (typeof console !== 'undefined')
-            console.log('Firebase data: ', ref.toString());
-          return ref;
-        }
-        init();
+        });
+            
 
-    };
+    });
     testCourse2 = new EJS({url: 'views/dialog'}).render("123");
 
     $('#addNewCourseBtn').click(function(event) {
@@ -144,10 +145,29 @@ $(document).ready(function() {
     $('.CourseAddNote').click(function(event) {
         console.log("hello world:"+this.id);
 
-        var courseID = document.getElementById('AddNoteInputCourseName');
+        var courseID = document.getElementById('AddNoteInputCourseID');
+        var courseName = document.getElementById("AddNoteInputCourseName");
+
 
         courseID.value = this.id;
 
         $('#addNoteModal').modal('toggle');
     });
+
+    var init = (function(){
+        var regDiaLoader = (function() {
+            if(isRegCompletely===true){
+
+            }else{
+                console.log("showReg");
+                $('#myModal').modal('toggle');
+                // $('#example').tooltip(options)
+
+            }
+        }());
+
+        //cocoPad init
+        cocoPadRef = noteDataBaseRegister.getExampleRef();
+
+    }());
 });
