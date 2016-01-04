2f21c4f510ea96ecfbbdc81b96eb33de990456f3
tonyk <jinniw43805@gmail.com>
2016-01-04 15:20:17 +0800

    callback url

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '163589830669545', // your App ID
         'clientSecret'  : 'd804e65b96e8a26aa54119f490955275', // your App Secret
-        'callbackURL'   : 'http://localhost:3000/auth/facebook/callback'
+        'callbackURL'   : 'https://cryptic-plains-1033.herokuapp.com/auth/facebook/callback'
     },
 };
